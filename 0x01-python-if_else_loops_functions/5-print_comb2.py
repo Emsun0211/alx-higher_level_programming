7f1113810a7904ab14a2d8b243f4581d735dfe0e
Emsun0211 <emsunconcept@gmail.com>
2022-05-31 14:28:24 +0100

    6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for num in range(99):
-    if num < 10:
-        print("0{}, ".format(num), end="")
-    else:
+for num in range(100):
+    if num < 99:
         print("{}, ".format(num), end="")
+    else:
+        print("{:02}, ".format(num), end=", ")
 
